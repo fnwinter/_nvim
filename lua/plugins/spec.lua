1b14b51901a7317ef620e507b61e6b49e951efac
JungJik Lee <fnwinter@gmail.com>
2025-03-24 00:15:49 +0900

    add tele

--- lua/plugins/spec.lua
+++ lua/plugins/spec.lua
@@ -91,5 +91,13 @@ return {
   opts = {
     -- fill any relevant options here
   },
-  }
+  },
+
+
+ {
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
+-- or                              , branch = '0.1.x',
+      dependencies = { 'nvim-lua/plenary.nvim' }
+    }, 
+
 }
